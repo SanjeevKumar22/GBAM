5b11221400a1099f12ed4f39761c963727925f42
gk24015 <gk24015@gmail.com>
2022-11-04 17:45:28 +0530

    Repository updated

--- src/main/java/com/wellsfargo/bank/repository/CustomerRepository.java
+++ src/main/java/com/wellsfargo/bank/repository/CustomerRepository.java
@@ -1,5 +1,11 @@
 package com.wellsfargo.bank.repository;
 
-public class CustomerRepository {
+import com.wellsfargo.bank.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,String> {
+ 
 }
+
--- src/main/java/com/wellsfargo/bank/service/CustomerService.java
+++ src/main/java/com/wellsfargo/bank/service/CustomerService.java
@@ -1,5 +1,29 @@
 package com.wellsfargo.bank.service;
+import com.wellsfargo.bank.model.Customer;
+import com.wellsfargo.bank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.List;
+@Service
 public class CustomerService {
-
+@Autowired
+private CustomerRepository customerRepository;
+public List<Customer> listAll(){
+	return customerRepository.findAll();}
+	
+public void save(Customer customer) {
+	customerRepository.save(customer);
 }
+
+public Customer get(String customer_number) {
+	return customerRepository.findById(customer_number).get();
+}
+
+public void delete(String customer_number) {
+	customerRepository.deleteById(customer_number);
+}
+}
+
