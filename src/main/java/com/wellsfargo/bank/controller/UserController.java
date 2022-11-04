38998fd70130013bc858132c66292557e58507e4
Sanjeev <msanjeev.200@gmail.com>
2022-11-04 16:36:11 +0530

    admin completed

--- src/main/java/com/wellsfargo/bank/controller/UserController.java
+++ src/main/java/com/wellsfargo/bank/controller/UserController.java
@@ -3,19 +3,16 @@ package com.wellsfargo.bank.controller;
 import com.wellsfargo.bank.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @ResponseBody
 public class UserController {
 
-   UserService userService = new UserService();
+   @Autowired
+   UserService userService;
    @GetMapping("/login")
-   @CrossOrigin(origins = "http://localhost:8080")
-   public Boolean validateUser(String userName, String password){
-   return userService.validate(userName,password);
+   public Boolean validateUser(@RequestParam("uname") String userName, @RequestParam("password") String password){
+      return userService.validate(userName,password);
    }
 }
