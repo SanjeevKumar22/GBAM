7938f90f85fb4b36f42b7612cea5bcc5a75aaada
dds-dhruvi <dhruvids@gmail.com>
2022-11-04 16:59:03 +0530

    Changes in frontend

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -2,7 +2,8 @@ import React, {Fragment,useEffect} from 'react';
 import  { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {Login} from './Component/Login';
-import {Menu} from './Component/Menu'
+import {Menu} from './Component/Menu';
+import {AddAccount} from './Component/AddAccount';
 
 import logo from './logo.svg';
 import './App.css';
@@ -14,6 +15,7 @@ const App=()=> {
       <Routes>
       <Route exact path="/" element={<Login/>}/>
       <Route exact path="/menu" element={<Menu/>}/>
+      <Route exact path ="/menu/AddAccount" element ={<AddAccount />} />
       </Routes>
     </Fragment>
    </Router>
--- src/main/ui/src/Component/Login.js
+++ src/main/ui/src/Component/Login.js
@@ -1,4 +1,101 @@
+import React, { Suspense, useState } from "react";
+import ReactDOM from "react-dom";
+import Menu from "./Menu";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./Login.css";
+// import { UseNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+
 export const Login=()=>{
-    return(
-    <div>Login page</div>);
-} 
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  // const navigate = UseNavigate();
+
+  // User Login info
+  const database = [
+    {
+      username: "dhruvi",
+      password: "pass1"
+    },
+
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+        return <Navigate replace to="/Menu" />
+
+        
+
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {/* {isSubmitted ? <div>User is successfully logged in</div> : renderForm} */}
+        {isSubmitted ?  <Navigate replace to= "menu" />: renderForm}
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
